701f61f6db42b04827f8cd7b2dd0357fad905a4d
udehnnamdi <udehnnamdi.edu@gmail.com>
2021-07-14 12:34:57 +0200

    init

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,8 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-// const socket = io('http://localhost:5000');
-const socket = io('http://localhost:5000');
+const socket = io('https://clips-video-chat.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
